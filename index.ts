129a52c6123853dce2f1ced4e7793ffe279ffb5e
Michael Sterenberg <michast@microsoft.com>
2026-05-20 11:06:52 +0300

    Add sample application code and CI/CD pipelines

---
+++ index.ts
@@ -0,0 +1,4 @@
+﻿interface User { id: number; name: string; }
+function greet(name: string): string { return `Hello, ${name}!`; }
+const user: User = { id: 1, name: "John" };
+console.log(greet(user.name));
